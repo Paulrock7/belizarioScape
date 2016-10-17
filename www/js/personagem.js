f7cf9a30cc48e80fb0c02df7e07609b2f49da579
pauloh <paulohp97@live.com>
2016-10-17 16:33:22 -0200

    commit

--- www/js/personagem.js
+++ www/js/personagem.js
@@ -1,7 +1,7 @@
 var xPersonagem=window.innerWidth/1.34;
 var yPersonagem=window.innerHeight/1.3;
 
-var velocidade = window.innerHeight/36;
+var velocidade = window.innerHeight/10;
 
 var xComida=window.innerWidth/2;
 var yComida=window.innerHeight/2;
